10d13b849a8a06cf05db17541316f30af2a370a1
vivianlee03 <vivian@u.sogang.ac.kr>
2023-07-11 12:32:45 +0900

    Create Account & See Profile

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+import schema from "./schema";
+import { ApolloServer } from "apollo-server";
+
+const server = new ApolloServer({
+  schema,
+});
+
+const PORT = process.env.PORT;
+
+server
+  .listen(PORT)
+  .then(() => console.log(`🌿 Server is running on http://localhost:${PORT}/`));
